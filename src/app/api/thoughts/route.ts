92baf2b900c06f1b2dcea502b8d99689c5fca68d
majolopez <majolopez1991@gmail.com>
2025-03-21 16:16:41 -0500

    adding logs for error in POST api/thoughts

--- src/app/api/thoughts/route.ts
+++ src/app/api/thoughts/route.ts
@@ -34,6 +34,7 @@ export async function POST(req: Request) {
 
     return NextResponse.json(newThought, { status: 201 });
   } catch (error) {
+    console.log(error)
     return NextResponse.json({ error: "Failed to save thought" }, { status: 500 });
   }
 }
